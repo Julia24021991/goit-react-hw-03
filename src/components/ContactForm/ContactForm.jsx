ffa35a2176222261b64527f0184c8e265c9a84ef
Julia24021991 <yuliya.kovalyk@gmail.com>
2024-02-02 12:09:26 +0200

    validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,18 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useId } from "react";
+import * as Yup from "yup";
+
+const contactSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(3, "Too short! Min 3 symbols")
+        .max(50, "Too long! Max 50 symbols")
+        .required("Required"),
+    number: Yup.string()
+        .min(3, "Too short! Min 3 symbols")
+        .max(50, "Too long! Max 50 symbols")
+        .required("Required"),
+})
+
 
 export const ContactForm = ({ onAdd }) => {
     const nameFieldId = useId();
@@ -13,6 +26,8 @@ export const ContactForm = ({ onAdd }) => {
                     number: ""
                 }
             }
+            validationSchema={contactSchema}
+
             onSubmit={(values, actions) => {
                 console.log(values)
                 onAdd({ id: Date.now(), ...values })
@@ -23,13 +38,13 @@ export const ContactForm = ({ onAdd }) => {
                 <div>
                     <label htmlFor={nameFieldId}>Name</label>
                     <Field type="text" name="name" id={nameFieldId} />
-                    <ErrorMessage name="name" />
+                    <ErrorMessage name="name" component="span" />
 
                 </div>
                 <div>
                     <label htmlFor={numberFieldId}>Number</label>
                     <Field type="text" name="number" id={numberFieldId} />
-                    <ErrorMessage name="number" />
+                    <ErrorMessage name="number" component="span" />
 
                 </div>
                 <button type="submit">Add contacts</button>
